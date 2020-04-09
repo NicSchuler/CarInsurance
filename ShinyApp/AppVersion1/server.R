c8d79171209291fa687a1eff89b5fde5fcbfd2fb
NicSchuler <56020278+NicSchuler@users.noreply.github.com>
2020-04-09 15:23:51 +0200

    Working on Shiny version 1

--- ShinyApp/AppVersion1/server.R
+++ ShinyApp/AppVersion1/server.R
@@ -1,11 +1,34 @@
 library(shiny)
 library(DT)
 library(data.table)
+library(ggplot2)
 
-traindata = fread("../../Data/pg17traindata.csv")
+# Load the data and change the claims counter (Number not amount) to a factor 
+traindata = fread("../../Data/pg17traindata.csv", stringsAsFactors = TRUE)
+traindata$CountDistinct_id_claim = as.factor(traindata$CountDistinct_id_claim)
 
 # Build the server
 shinyServer(function(input, output) {
+    
+    # Filter the data according to "Filter"-Panel
+    filtered = reactive({
+        filtered = traindata
+        filtered
+    })
+    
+    output$Scatterplot = renderPlot({
+        Scatter = ggplot(filtered(), aes_string(x=input$Scatter_X_Axis, y=input$Scatter_Y_Axis, color=input$Scatter_Color, size=input$Scatter_Size)) +
+            geom_point()
+        ScatterSmooth = Scatter +
+            geom_smooth(aes_string(x=input$Scatter_X_Axis, y=input$Scatter_Y_Axis), formula=y~x,na.rm=TRUE) +
+            theme_classic()
+        if(input$Scatter_Smooth == "Yes"){
+            return(ScatterSmooth)
+        }
+        else{
+            return(Scatter + theme_classic())
+        }
+    })
 
 
 })
--- ShinyApp/AppVersion1/ui.R
+++ ShinyApp/AppVersion1/ui.R
@@ -1,34 +1,70 @@
 library(shiny)
+library(tidyverse)
 
+traincols = colnames(traindata)
 
+numcols = c("pol_bonus", "pol_duration", "drv_age1", "drv_age2", "drv_age_lic1", "drv_age_lic2", "vh_age", "vh_cyl", "vh_speed", "vh_value", "vh_weight", "Sum_claim_amount")
 
-# Define UI for application
+factorcols = c("pol_coverage", "pol_pay_freq", "pol_payd", "pol_usage", "drv_drv2", "drv_sex1", "drv_sex2", "vh_fuel", "vh_make", "vh_type", "CountDistinct_id_claim")
+
+# Define UI for application-----------
 shinyUI(fluidPage(
     navbarPage("BSG Insurance",
-               # First Main-Tab
+               # First Main-Tab------------
                tabPanel("Explore Training Data",
                         tabsetPanel(
-                            # Fist Sub-Tab
-                            tabPanel("Plots",
+                            # First Sub-Tab
+                            tabPanel("Filters",
+                                     br(),
+                                     flowLayout(
+                                             
+                                         )
+                            ),
+                            
+                            # Second Sub-Tab
+                            tabPanel("Scatterplot",
+                                     br(),
+                                     sidebarLayout(
+                                         sidebarPanel(
+                                             p(strong("Variables for the Plot")),
+                                             selectInput("Scatter_X_Axis", "Variable for X-Axis", choices = numcols, selected = "vh_speed"),
+                                             selectInput("Scatter_Y_Axis", "Variable for Y-Axis", choices = numcols, selected = "Sum_claim_amount"),
+                                             selectInput("Scatter_Color", "Variable for color", choices = factorcols, selected = "vh_value"),
+                                             selectInput("Scatter_Size", "Variable for size", choices = numcols, selected = "CountDistinct_id_claim"),
+                                             radioButtons("Scatter_Smooth", "Add Regression", choices = c("Yes", "No"), selected = "No")
+                                             ),
+                                         mainPanel(
+                                             plotOutput("Scatterplot")
+                                         )
+                                         )
+                                     ),
+                            
+                            # Third Sub-Tab
+                            tabPanel("Data Table",
                                      br(),
                                      sidebarLayout(
                                          sidebarPanel(
                                              
-                                             )
+                                         ),
+                                         mainPanel(
+                                             
                                          )
-                                     )),
-                            
-                            # Second Sub-Tab
-                            tabPanel("Data Table")
+                                     )
+                                     )
+                        
                         )),
                
-               # Second Main-Tab
+               
+               # Second Main-Tab-------------
                tabPanel("Policy Portfolio Management"),
                
-               # Third Main-Tab
+               
+               # Third Main-Tab------------
                tabPanel("Premium Calculator"),
                
-               # Fourth Main-Tab
-               tabPanel("Additional Explanations"))
+               
+               # Fourth Main-Tab----------
+               tabPanel("Additional Explanations")
+               )
    
 ))
