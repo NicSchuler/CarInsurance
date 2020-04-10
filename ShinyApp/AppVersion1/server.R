341f749375407e86109fdcbf6ffe1dd9c640375b
NicSchuler <56020278+NicSchuler@users.noreply.github.com>
2020-04-10 10:40:49 +0200

    Working on Shiny App Version 1

--- ShinyApp/AppVersion1/server.R
+++ ShinyApp/AppVersion1/server.R
@@ -6,6 +6,7 @@ library(ggplot2)
 # Load the data and change the claims counter (Number not amount) to a factor 
 traindata = fread("../../Data/pg17traindata.csv", stringsAsFactors = TRUE)
 traindata$CountDistinct_id_claim = as.factor(traindata$CountDistinct_id_claim)
+traindata$drv_drv2 = factor(traindata$drv_drv2, labels=c("1 Driver", "2 Drivers"))
 
 # Build the server
 shinyServer(function(input, output) {
@@ -17,17 +18,14 @@ shinyServer(function(input, output) {
     })
     
     output$Scatterplot = renderPlot({
-        Scatter = ggplot(filtered(), aes_string(x=input$Scatter_X_Axis, y=input$Scatter_Y_Axis, color=input$Scatter_Color, size=input$Scatter_Size)) +
+        Scatter = ggplot(filtered(), aes_string(x=input$Scatter_X_Axis, y=input$Scatter_Y_Axis, color=input$Scatter_Color, shape=input$Scatter_Shape)) +
             geom_point()
         ScatterSmooth = Scatter +
-            geom_smooth(aes_string(x=input$Scatter_X_Axis, y=input$Scatter_Y_Axis), formula=y~x,na.rm=TRUE) +
+            geom_smooth(formula = y~x, na.rm=TRUE) +
             theme_classic()
-        if(input$Scatter_Smooth == "Yes"){
-            return(ScatterSmooth)
-        }
-        else{
-            return(Scatter + theme_classic())
-        }
+        ScSm = input$Scatter_Smooth
+        
+        ifelse(ScSm == "Yes", return(ScatterSmooth), return(Scatter + theme_classic()))
     })
 
 
--- ShinyApp/AppVersion1/ui.R
+++ ShinyApp/AppVersion1/ui.R
@@ -5,7 +5,7 @@ traincols = colnames(traindata)
 
 numcols = c("pol_bonus", "pol_duration", "drv_age1", "drv_age2", "drv_age_lic1", "drv_age_lic2", "vh_age", "vh_cyl", "vh_speed", "vh_value", "vh_weight", "Sum_claim_amount")
 
-factorcols = c("pol_coverage", "pol_pay_freq", "pol_payd", "pol_usage", "drv_drv2", "drv_sex1", "drv_sex2", "vh_fuel", "vh_make", "vh_type", "CountDistinct_id_claim")
+factorcols = c("NULL", "pol_coverage", "pol_pay_freq", "pol_payd", "pol_usage", "drv_drv2", "drv_sex1", "drv_sex2", "vh_fuel", "vh_make", "vh_type", "CountDistinct_id_claim")
 
 # Define UI for application-----------
 shinyUI(fluidPage(
@@ -16,9 +16,26 @@ shinyUI(fluidPage(
                             # First Sub-Tab
                             tabPanel("Filters",
                                      br(),
+                                     p(strong("These filters are applied to all Plots and the data.table of the training data")),
+                                     submitButton("Apply Filters"),
+                                     br(),
+                                     p(strong("Filter about the policies")),
                                      flowLayout(
-                                             
-                                         )
+                                       sliderInput("Filter_Bonus", "Filter pol_bonus", min=0.5, max=3.5, value=c(0.5, 3.5)),
+                                       checkboxGroupInput("Filter_coverage", "Filter pol_coverage", c("Mini", "Median1", "Median2", "Maxi"), selected=c("Mini", "Median1", "Median2", "Maxi"), inline=TRUE),
+                                       checkboxGroupInput("Filter_usage", "Filter pol_usage", c("Retired", "WorkPrivate", "Professional", "AllTrips"), selected=c("Retired", "WorkPrivate", "Professional", "AllTrips"), inline=TRUE)
+                                     ),
+                                     br(),
+                                     p(strong("Filter about the drivers / policyholders")),
+                                     flowLayout(
+                                       checkboxGroupInput("Filter_drv_drv2","Filter drv_drv2", c("1 Driver", "2 Drivers"), selected=c("1 Driver", "2 Drivers"), inline=TRUE),
+                                       sliderInput("Filter_drv_age1", "Filter drv_age1", min=18, max=105, value=c(18,105)),
+                                       sliderInput("Filter_drv_age2", "Filter drv_age2", min=18, max=105, value=c(18,105)),
+                                       checkboxGroupInput("Filter_drv_sex1", "Filter drv_sex1", c("M","F"), selected=c("M","F"), inline=TRUE),
+                                       checkboxGroupInput("Filter_drv_sex2", "Filter drv_sex2", c("M","F"), selected=c("M","F"), inline=TRUE),
+                                       sliderInput("Filter_drv_age_lic1", "Filter drv_age_lic1", min=0, max=115, value=c(0,115)),
+                                       sliderInput("Filter_drv_age_lic2", "Filter drv_age_lic2", min=0, max=115, value=c(0,115))
+                                     )
                             ),
                             
                             # Second Sub-Tab
@@ -29,12 +46,15 @@ shinyUI(fluidPage(
                                              p(strong("Variables for the Plot")),
                                              selectInput("Scatter_X_Axis", "Variable for X-Axis", choices = numcols, selected = "vh_speed"),
                                              selectInput("Scatter_Y_Axis", "Variable for Y-Axis", choices = numcols, selected = "Sum_claim_amount"),
-                                             selectInput("Scatter_Color", "Variable for color", choices = factorcols, selected = "vh_value"),
-                                             selectInput("Scatter_Size", "Variable for size", choices = numcols, selected = "CountDistinct_id_claim"),
-                                             radioButtons("Scatter_Smooth", "Add Regression", choices = c("Yes", "No"), selected = "No")
+                                             selectInput("Scatter_Color", "Variable for color", choices = factorcols, selected = "NULL"),
+                                             selectInput("Scatter_Shape", "Variable for shape", choices = factorcols, selected = "NULL"),
+                                             radioButtons("Scatter_Smooth", "Add Regression (only possible if shape = NULL)", choices = c("Yes", "No"), selected = "No", inline = TRUE),
+                                             submitButton("Apply")
                                              ),
                                          mainPanel(
-                                             plotOutput("Scatterplot")
+                                           p(strong("Scatterplot filtered according to Filter-Page")),
+                                           br(),
+                                           plotOutput("Scatterplot")
                                          )
                                          )
                                      ),
