1f4e9c15b01e6ff78bbd464af96804d81b5e15b1
NicSchuler <56020278+NicSchuler@users.noreply.github.com>
2020-04-22 20:06:16 +0200

    Working on App V2

--- ShinyApp/AppVersion2/server.R
+++ ShinyApp/AppVersion2/server.R
@@ -3,6 +3,7 @@ library(shiny)
 library(DT)
 library(data.table)
 library(ggplot2)
+library(tidyverse)
 library(stringr)
 library(rgdal)      # Reading and projecting shapefiles
 library(plotrix)    # Creating color scales
@@ -18,6 +19,18 @@ MatchedData$claim = as.factor(ifelse(MatchedData$Sum_claim_amount > 0, 1,0))
 MatchedData$pol_insee_code = as.character(MatchedData$pol_insee_code)
 MatchedData$Departement = as.factor(str_sub(MatchedData$pol_insee_code, end=-4))
 
+load("../../Data/pg17testyear4.Rdata")
+pg17testyear4 = setDT(pg17testyear4)
+pg17testyear4$drv_drv2 = factor(pg17testyear4$drv_drv2, labels=c("1 Driver", "2 Drivers"))
+pg17testyear4$pol_insee_code = as.character(pg17testyear4$pol_insee_code)
+pg17testyear4$Departement = as.factor(str_sub(pg17testyear4$pol_insee_code, end=-4))
+
+# Create a subsample for the portfolio management tab
+set.seed(1)
+pg17testyear4corr = sample_n(pg17testyear4, 20000, replace=FALSE)
+pg17testyear4corr = setDT(pg17testyear4corr)
+
+
 # Load the data für the map
 departements <- readOGR(dsn="../Geofla/Useful/DEPARTEMENT.shp")
 departements@data$id = rownames(departements@data)
@@ -31,8 +44,8 @@ departements.dt = setDT(departements.df)
 # Build the server
 shinyServer(function(input, output) {
     
-    # Data Filter------------
-    # Filter the data according to "Filter"-Panel
+    # Data Filters------------
+    # Filter the traindata according to "Filter"-Panel
     filtered = eventReactive(input$Filter_Go, {
         MatchedData[pol_bonus >= input$Filter_pol_bonus[1] & pol_bonus <= input$Filter_pol_bonus[2] &
                   pol_coverage %in% input$Filter_pol_coverage &
@@ -54,6 +67,28 @@ shinyServer(function(input, output) {
                   CountDistinct_id_claim %in% input$Filter_CountDistinct_id_claim &
                   Sum_claim_amount >= input$Filter_Sum_claim_amount[1] & Sum_claim_amount <= input$Filter_Sum_claim_amount[2]]
                   
+    })
+    
+    # Filter the testyear1 data according to "Filter"-Panel
+    filteredtest4 = eventReactive(input$Filter_Go, {
+        pg17testyear4corr[pol_bonus >= input$Filter_pol_bonus[1] & pol_bonus <= input$Filter_pol_bonus[2] &
+                        pol_coverage %in% input$Filter_pol_coverage &
+                        pol_usage %in% input$Filter_pol_usage &
+                        drv_drv2 %in% input$Filter_drv_drv2 &
+                        drv_age1 >= input$Filter_drv_age1[1] & drv_age1 <= input$Filter_drv_age1[2] &
+                        (drv_age2 >= input$Filter_drv_age2[1] & drv_age2 <= input$Filter_drv_age2[2] | drv_age2 == 0) &
+                        drv_sex1 %in% input$Filter_drv_sex1 &
+                        drv_sex2 %in% c(input$Filter_drv_sex2, 0) &
+                        drv_age_lic1 >= input$Filter_drv_age_lic1[1] & drv_age_lic1 <= input$Filter_drv_age_lic1[2] &
+                        (drv_age_lic2 >= input$Filter_drv_age_lic2[1] & drv_age_lic2 <= input$Filter_drv_age_lic2[2] | drv_age_lic2 == 0) &
+                        vh_age >= input$Filter_vh_age[1] & vh_age <= input$Filter_vh_age[2] &
+                        vh_cyl >= input$Filter_vh_cyl[1] & vh_cyl <= input$Filter_vh_cyl[2] &
+                        vh_din >= input$Filter_vh_din[1] & vh_din <= input$Filter_vh_din[2] &
+                        vh_speed >= input$Filter_vh_speed[1] & vh_speed <= input$Filter_vh_speed[2] &
+                        vh_type %in% input$Filter_vh_type &
+                        vh_weight >= input$Filter_vh_weight[1] & vh_weight <= input$Filter_vh_weight[2] &
+                        vh_value >= input$Filter_vh_value[1] & vh_value <= input$Filter_vh_value[2]]
+        
     })
     
     # Scatterplot - Output-----------
@@ -144,6 +179,34 @@ shinyServer(function(input, output) {
             ggsave(file,MapPlot())
         }
     )
-
-
+    
+    # Compare Densities Plot - Output--------
+    DensTrain = eventReactive(input$ComDensity_Go, {
+        a = as.data.frame(filtered())
+        aa = density(a[,(input$CompDensity_Var)])
+        DensTrain = aa$y})
+    DensTest = eventReactive(input$ComDensity_Go, {
+        b = as.data.frame(filteredtest4())
+        bb = density(b[,(input$CompDensity_Var)])
+        DensTest = bb$y})
+    DensTestX = eventReactive(input$ComDensity_Go, {
+        c = as.data.frame(filteredtest4())
+        cc = density(c[,(input$CompDensity_Var)])
+        DensTestX = cc$x})
+    
+    DensDf = reactive({
+        DensDf = data.frame(
+            x=DensTestX(),
+            y=(DensTest()-DensTrain())*nrow(filteredtest4())
+        )
+    })
+    
+    output$ComDensPlot = renderPlot({
+        ggplot(DensDf(), aes(x=x, y=y, color = y)) +
+            geom_line(size=1.5) +
+            scale_color_gradient2(midpoint=0, low="red", mid="darkgrey", high="green4", space ="Lab") +
+            labs(x=paste(input$CompDensity_Var), y="Deviance from Trainig-Density") +
+            theme_classic()
+    })
+    
 })
--- ShinyApp/AppVersion2/ui.R
+++ ShinyApp/AppVersion2/ui.R
@@ -3,8 +3,6 @@ library(shiny)
 library(tidyverse)
 
 # Drop-Down Lists-----------
-traincols = colnames(traindata)
-
 numcols = c("pol_bonus", "pol_duration", "drv_age1", "drv_age2", "drv_age_lic1", "drv_age_lic2", "vh_age", "vh_cyl", "vh_din", "vh_speed", "vh_value", "vh_weight", "Sum_claim_amount")
 
 factorcols = c("NULL", "pol_coverage", "pol_pay_freq", "pol_payd", "pol_usage", "drv_drv2", "drv_sex1", "drv_sex2", "vh_fuel", "vh_make", "vh_type", "CountDistinct_id_claim")
@@ -15,72 +13,71 @@ MapVars = c("insured_Cars", "Avg_pol_bonus", "Avg_claim_count", "Avg_claim_amoun
 
 shinyUI(fluidPage(
     navbarPage("BSGN Insurance",
-               # First Main-Tab------------
+               # First Main-Tab Filters---------
+               tabPanel("Filters",
+                        br(),
+                        p(strong("These filters are applied to all Plots and the data.table of the training data")),
+                        br(),
+                        
+                        # Filter policies
+                        radioButtons("Filter_pol", "Filter Policy-Types", choices = c("Yes", "No"), selected = "No", inline = TRUE),
+                        conditionalPanel(
+                          condition = "input.Filter_pol == 'Yes'",
+                          flowLayout(
+                            sliderInput("Filter_pol_bonus", "Filter pol_bonus", min=0.5, max=3.5, value=c(0.5, 3.5)),
+                            checkboxGroupInput("Filter_pol_coverage", "Filter pol_coverage", c("Mini", "Median1", "Median2", "Maxi"), selected=c("Mini", "Median1", "Median2", "Maxi"), inline=TRUE),
+                            checkboxGroupInput("Filter_pol_usage", "Filter pol_usage", c("Retired", "WorkPrivate", "Professional", "AllTrips"), selected=c("Retired", "WorkPrivate", "Professional", "AllTrips"), inline=TRUE)
+                          )
+                        ),
+                        br(),
+                        
+                        # Filter Drivers / Policy-Holders
+                        radioButtons("Filter_drv", "Filter Drivers / Policy-Holders", choices = c("Yes", "No"), selected = "No", inline = TRUE),
+                        conditionalPanel(
+                          condition = "input.Filter_drv == 'Yes'",
+                          flowLayout(
+                            checkboxGroupInput("Filter_drv_drv2","Filter drv_drv2", c("1 Driver", "2 Drivers"), selected=c("1 Driver", "2 Drivers"), inline=TRUE),
+                            sliderInput("Filter_drv_age1", "Filter drv_age1", min=18, max=105, value=c(18,105)),
+                            sliderInput("Filter_drv_age2", "Filter drv_age2", min=18, max=105, value=c(18,105)),
+                            checkboxGroupInput("Filter_drv_sex1", "Filter drv_sex1", c("M","F"), selected=c("M","F"), inline=TRUE),
+                            checkboxGroupInput("Filter_drv_sex2", "Filter drv_sex2", c("M","F"), selected=c("M","F"), inline=TRUE),
+                            sliderInput("Filter_drv_age_lic1", "Filter drv_age_lic1", min=0, max=115, value=c(0,115)),
+                            sliderInput("Filter_drv_age_lic2", "Filter drv_age_lic2", min=0, max=115, value=c(0,115))
+                          )
+                        ),
+                        br(),
+                        
+                        # Filter insured cars
+                        radioButtons("Filter_vh", "Filter insured cars", choices = c("Yes", "No"), selected = "No", inline = TRUE),
+                        conditionalPanel(
+                          condition = "input.Filter_vh == 'Yes'",
+                          flowLayout(
+                            sliderInput("Filter_vh_age", "Filter vh_age", min=0, max=70, value=c(0,70)),
+                            sliderInput("Filter_vh_cyl", "Filter vh_cyl", min=0, max=7000, value=c(0,7000)),
+                            sliderInput("Filter_vh_din", "Filter vh_din", min=0, max=600, value=c(0,600)),
+                            sliderInput("Filter_vh_speed", "Filter vh_speed", min=20, max=320, value=c(20,320)),
+                            checkboxGroupInput("Filter_vh_type", "Filter vh_type", c("Tourism","Commercial"), selected=c("Tourism","Commercial"), inline=TRUE),
+                            sliderInput("Filter_vh_value", "Filter vh_value", min=0, max=160000, value=c(0,160000)),
+                            sliderInput("Filter_vh_weight", "Filter vh_weight", min=0, max=8000, value=c(0,8000))
+                            
+                          )),
+                        br(),
+                        
+                        # Filter insured cars
+                        radioButtons("Filter_claims", "Filter the Claims", choices = c("Yes", "No"), selected = "No", inline = TRUE),
+                        conditionalPanel(
+                          condition = "input.Filter_claims == 'Yes'",
+                          flowLayout(
+                            checkboxGroupInput("Filter_CountDistinct_id_claim", "Filter CountDistinct_id_claim", c(0:9), selected=c(0:9), inline=TRUE),
+                            sliderInput("Filter_Sum_claim_amount", "Filter Sum_claim_amount", min=-10000, max=150000, value=c(-10000,150000))
+                          )),
+                        
+                        actionButton("Filter_Go", "Apply Filter")
+               ),
+               # Second Main-Tab Trainingdata------------
                tabPanel("Explore Training Data",
                         tabsetPanel(
-                            # First Sub-Tab, Filters---------
-                            tabPanel("Filters",
-                                     br(),
-                                     p(strong("These filters are applied to all Plots and the data.table of the training data")),
-                                     br(),
-                                     
-                                     # Filter policies
-                                     radioButtons("Filter_pol", "Filter Policy-Types", choices = c("Yes", "No"), selected = "No", inline = TRUE),
-                                     conditionalPanel(
-                                       condition = "input.Filter_pol == 'Yes'",
-                                     flowLayout(
-                                       sliderInput("Filter_pol_bonus", "Filter pol_bonus", min=0.5, max=3.5, value=c(0.5, 3.5)),
-                                       checkboxGroupInput("Filter_pol_coverage", "Filter pol_coverage", c("Mini", "Median1", "Median2", "Maxi"), selected=c("Mini", "Median1", "Median2", "Maxi"), inline=TRUE),
-                                       checkboxGroupInput("Filter_pol_usage", "Filter pol_usage", c("Retired", "WorkPrivate", "Professional", "AllTrips"), selected=c("Retired", "WorkPrivate", "Professional", "AllTrips"), inline=TRUE)
-                                     )
-                                     ),
-                                     br(),
-                                     
-                                     # Filter Drivers / Policy-Holders
-                                     radioButtons("Filter_drv", "Filter Drivers / Policy-Holders", choices = c("Yes", "No"), selected = "No", inline = TRUE),
-                                     conditionalPanel(
-                                       condition = "input.Filter_drv == 'Yes'",
-                                     flowLayout(
-                                       checkboxGroupInput("Filter_drv_drv2","Filter drv_drv2", c("1 Driver", "2 Drivers"), selected=c("1 Driver", "2 Drivers"), inline=TRUE),
-                                       sliderInput("Filter_drv_age1", "Filter drv_age1", min=18, max=105, value=c(18,105)),
-                                       sliderInput("Filter_drv_age2", "Filter drv_age2", min=18, max=105, value=c(18,105)),
-                                       checkboxGroupInput("Filter_drv_sex1", "Filter drv_sex1", c("M","F"), selected=c("M","F"), inline=TRUE),
-                                       checkboxGroupInput("Filter_drv_sex2", "Filter drv_sex2", c("M","F"), selected=c("M","F"), inline=TRUE),
-                                       sliderInput("Filter_drv_age_lic1", "Filter drv_age_lic1", min=0, max=115, value=c(0,115)),
-                                       sliderInput("Filter_drv_age_lic2", "Filter drv_age_lic2", min=0, max=115, value=c(0,115))
-                                     )
-                                     ),
-                                     br(),
-                                     
-                                     # Filter insured cars
-                                     radioButtons("Filter_vh", "Filter insured cars", choices = c("Yes", "No"), selected = "No", inline = TRUE),
-                                     conditionalPanel(
-                                       condition = "input.Filter_vh == 'Yes'",
-                                     flowLayout(
-                                       sliderInput("Filter_vh_age", "Filter vh_age", min=0, max=70, value=c(0,70)),
-                                       sliderInput("Filter_vh_cyl", "Filter vh_cyl", min=0, max=7000, value=c(0,7000)),
-                                       sliderInput("Filter_vh_din", "Filter vh_din", min=0, max=600, value=c(0,600)),
-                                       sliderInput("Filter_vh_speed", "Filter vh_speed", min=20, max=320, value=c(20,320)),
-                                       checkboxGroupInput("Filter_vh_type", "Filter vh_type", c("Tourism","Commercial"), selected=c("Tourism","Commercial"), inline=TRUE),
-                                       sliderInput("Filter_vh_value", "Filter vh_value", min=0, max=160000, value=c(0,160000)),
-                                       sliderInput("Filter_vh_weight", "Filter vh_weight", min=0, max=8000, value=c(0,8000))
-                                       
-                                     )),
-                                     br(),
-                                     
-                                     # Filter insured cars
-                                     radioButtons("Filter_claims", "Filter the Claims", choices = c("Yes", "No"), selected = "No", inline = TRUE),
-                                     conditionalPanel(
-                                       condition = "input.Filter_claims == 'Yes'",
-                                     flowLayout(
-                                       checkboxGroupInput("Filter_CountDistinct_id_claim", "Filter CountDistinct_id_claim", c(0:9), selected=c(0:9), inline=TRUE),
-                                       sliderInput("Filter_Sum_claim_amount", "Filter Sum_claim_amount", min=-10000, max=150000, value=c(-10000,150000))
-                                     )),
-                                         
-                                     actionButton("Filter_Go", "Apply Filter")
-                            ),
-                            
-                            # Second Sub-Tab, Scatterplot-------------
+                            # First Sub-Tab, Scatterplot-------------
                             tabPanel("Scatterplot",
                                      br(),
                                      sidebarLayout(
@@ -110,7 +107,7 @@ shinyUI(fluidPage(
                                          )
                                      ),
                             
-                            # Third Sub-Tab, Map----------
+                            # Second Sub-Tab, Map----------
                             tabPanel("Map",
                                      br(),
                                      sidebarLayout(
@@ -129,7 +126,7 @@ shinyUI(fluidPage(
                                      )
                             ),
                             
-                            # Fourth Sub-Tab, Data Table------------
+                            # Third Sub-Tab, Data Table------------
                             tabPanel("Data Table",
                                      br(),
                                      mainPanel(
@@ -140,15 +137,51 @@ shinyUI(fluidPage(
                         )),
                
                
-               # Second Main-Tab-------------
-               tabPanel("Policy Portfolio Management"),
+               # Third Main-Tab Policy Protfolio Management-------------
+               tabPanel("Policy Portfolio Management",
+                        tabsetPanel(
+                          
+                          # First Sub-Tab, Densities---------
+                          tabPanel("Densities",
+                                   br(),
+                                   sidebarLayout(
+                                     sidebarPanel(
+                                       p(strong("Choose the Variable you want to compare")),
+                                       br(),
+                                       selectInput("CompDensity_Var", "Variable to compare", choices = numcols, selected = "vh_speed"),
+                                       br(),
+                                       actionButton("ComDensity_Go", "Create Plot")
+                                     ),
+                                     mainPanel(
+                                       p(strong("Compare densites of training data with actual policies")),
+                                       p("Both data sets are filtered according to the filter tab."),
+                                       br(),
+                                       plotOutput("ComDensPlot")
+                                     )
+                        )),
+                        
+                        # Second Sub-Tab, Categories---------
+                        tabPanel("Categories",
+                                 br(),
+                                 sidebarLayout(
+                                   sidebarPanel(
+                                     p(strong("Choose the Variable you want to compare"))
+                                   ),
+                                   mainPanel(
+                                     p(strong("Compare densites of training data with actual policies")),
+                                     br(),
+                                     p("Both data sets are filtered according to the filter tab.")
+                                   )
+                                 ))
+                        
+                        
+                        )),
                
-               
-               # Third Main-Tab------------
+               # Fourth Main-Tab Premium Calculator------------
                tabPanel("Premium Calculator"),
                
                
-               # Fourth Main-Tab----------
+               # Fifth Main-Tab Additional Explanations----------
                tabPanel("Additional Explanations")
                )
    
